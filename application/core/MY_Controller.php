4a324a8d9b417c82284bf42894d2f131e1171832
kboranbayev <boranbayev.k@gmail.com>
2017-10-12 16:18:57 +0600

    Added Homepage features
    Signed-off-by: kboranbayev <boranbayev.k@gmail.com>

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -23,7 +23,7 @@ class Application extends CI_Controller
 
 		//  Set basic view parameters
 		$this->data = array ();
-		$this->data['pagetitle'] = 'TODO List Manager';
+		$this->data['pagetitle'] = 'TODO List Manager by Kuanysh and Michael';
 		$this->data['ci_version'] = (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>'.CI_VERSION.'</strong>' : '';
 	}
 
